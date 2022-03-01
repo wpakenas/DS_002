4caa9880ac08c2dedc3b825272b3e554308ab970
wpakenas <98054572+wpakenas@users.noreply.github.com>
2022-02-28 18:19:26 -0800

    Update linear_algebra.py

--- linear_algebra.py
+++ linear_algebra.py
@@ -2,11 +2,22 @@ from typing import List
 
 Vector = List[float]
 
+height_weight_age = [70,  # inches,
+                     170, # pounds,
+                     40 ] # years
+
+grades = [95,   # exam1
+          80,   # exam2
+          75,   # exam3
+          62 ]  # exam4
+
 def add(v: Vector, w: Vector) -> Vector:
     """Adds corresponding elements"""
     assert len(v) == len(w), "vectors must be the same length"
 
     return [v_i + w_i for v_i, w_i in zip(v, w)]
+
+assert add([1, 2, 3], [4, 5, 6]) == [5, 7, 9]
 
 def subtract(v: Vector, w: Vector) -> Vector:
     """Subtracts corresponding elements"""
@@ -14,6 +25,7 @@ def subtract(v: Vector, w: Vector) -> Vector:
 
     return [v_i - w_i for v_i, w_i in zip(v, w)]
 
+assert subtract([5, 7, 9], [4, 5, 6]) == [1, 2, 3]
 
 def vector_sum(vectors: List[Vector]) -> Vector:
     """Sums all corresponding elements"""
@@ -28,17 +40,20 @@ def vector_sum(vectors: List[Vector]) -> Vector:
     return [sum(vector[i] for vector in vectors)
             for i in range(num_elements)]
 
+assert vector_sum([[1, 2], [3, 4], [5, 6], [7, 8]]) == [16, 20]
 
 def scalar_multiply(c: float, v: Vector) -> Vector:
     """Multiplies every element by c"""
     return [c * v_i for v_i in v]
 
+assert scalar_multiply(2, [1, 2, 3]) == [2, 4, 6]
 
 def vector_mean(vectors: List[Vector]) -> Vector:
     """Computes the element-wise average"""
     n = len(vectors)
     return scalar_multiply(1/n, vector_sum(vectors))
 
+assert vector_mean([[1, 2], [3, 4], [5, 6]]) == [3, 4]
 
 def dot(v: Vector, w: Vector) -> float:
     """Computes v_1 * w_1 + ... + v_n * w_n"""
@@ -46,11 +61,13 @@ def dot(v: Vector, w: Vector) -> float:
 
     return sum(v_i * w_i for v_i, w_i in zip(v, w))
 
+assert dot([1, 2, 3], [4, 5, 6]) == 32  # 1 * 4 + 2 * 5 + 3 * 6
 
 def sum_of_squares(v: Vector) -> float:
     """Returns v_1 * v_1 + ... + v_n * v_n"""
     return dot(v, v)
 
+assert sum_of_squares([1, 2, 3]) == 14  # 1 * 1 + 2 * 2 + 3 * 3
 
 import math
 
@@ -58,6 +75,7 @@ def magnitude(v: Vector) -> float:
     """Returns the magnitude (or length) of v"""
     return math.sqrt(sum_of_squares(v))   # math.sqrt is square root function
 
+assert magnitude([3, 4]) == 5
 
 def squared_distance(v: Vector, w: Vector) -> float:
     """Computes (v_1 - w_1) ** 2 + ... + (v_n - w_n) ** 2"""
@@ -70,14 +88,89 @@ def distance(v: Vector, w: Vector) -> float:
 
 def distance(v: Vector, w: Vector) -> float:  # type: ignore
     return magnitude(subtract(v, w))
-  
-assert add([1, 2, 3], [10,9,8]) == [11,11,11], "something's wrong with add()"
-assert subtract([11,11,11], [1, 2, 3]) == [10,9,8], "trouble with subtract()"
-assert vector_sum([[3,4], [5,6], [7,8]]) == [15, 18], "vector_sum() problem"
-assert vector_mean([[1, 2], [3, 4], [5, 6]]) == [3, 4], "oopsie vector_mean()"
-assert dot([1, 2, 3], [4, 5, 6]) == 32, "dot() issue"  # 1 * 4 + 2 * 5 + 3 * 6
-assert sum_of_squares([1, 2, 3]) == 14, "sum_of_squares() fail"  # 1 * 1 + 2 * 2 + 3 * 3
-assert magnitude([3, 4]) == 5, "issue with magnitude()"
+
+# Another type alias
+Matrix = List[List[float]]
+
+A = [[1, 2, 3],  # A has 2 rows and 3 columns
+     [4, 5, 6]]
+
+B = [[1, 2],     # B has 3 rows and 2 columns
+     [3, 4],
+     [5, 6]]
+
+from typing import Tuple
+
+def shape(A: Matrix) -> Tuple[int, int]:
+    """Returns (# of rows of A, # of columns of A)"""
+    num_rows = len(A)
+    num_cols = len(A[0]) if A else 0   # number of elements in first row
+    return num_rows, num_cols
+
+assert shape([[1, 2, 3], [4, 5, 6]]) == (2, 3)  # 2 rows, 3 columns
+
+def get_row(A: Matrix, i: int) -> Vector:
+    """Returns the i-th row of A (as a Vector)"""
+    return A[i]             # A[i] is already the ith row
+
+def get_column(A: Matrix, j: int) -> Vector:
+    """Returns the j-th column of A (as a Vector)"""
+    return [A_i[j]          # jth element of row A_i
+            for A_i in A]   # for each row A_i
+
+from typing import Callable
+
+def make_matrix(num_rows: int,
+                num_cols: int,
+                entry_fn: Callable[[int, int], float]) -> Matrix:
+    """
+    Returns a num_rows x num_cols matrix
+    whose (i,j)-th entry is entry_fn(i, j)
+    """
+    return [[entry_fn(i, j)             # given i, create a list
+             for j in range(num_cols)]  #   [entry_fn(i, 0), ... ]
+            for i in range(num_rows)]   # create one list for each i
+
+def identity_matrix(n: int) -> Matrix:
+    """Returns the n x n identity matrix"""
+    return make_matrix(n, n, lambda i, j: 1 if i == j else 0)
+
+assert identity_matrix(5) == [[1, 0, 0, 0, 0],
+                              [0, 1, 0, 0, 0],
+                              [0, 0, 1, 0, 0],
+                              [0, 0, 0, 1, 0],
+                              [0, 0, 0, 0, 1]]
+
+data = [[70, 170, 40],
+        [65, 120, 26],
+        [77, 250, 19],
+        # ....
+       ]
+
+friendships = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
+               (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
+
+#            user 0  1  2  3  4  5  6  7  8  9
+#
+friend_matrix = [[0, 1, 1, 0, 0, 0, 0, 0, 0, 0],  # user 0
+                 [1, 0, 1, 1, 0, 0, 0, 0, 0, 0],  # user 1
+                 [1, 1, 0, 1, 0, 0, 0, 0, 0, 0],  # user 2
+                 [0, 1, 1, 0, 1, 0, 0, 0, 0, 0],  # user 3
+                 [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],  # user 4
+                 [0, 0, 0, 0, 1, 0, 1, 1, 0, 0],  # user 5
+                 [0, 0, 0, 0, 0, 1, 0, 0, 1, 0],  # user 6
+                 [0, 0, 0, 0, 0, 1, 0, 0, 1, 0],  # user 7
+                 [0, 0, 0, 0, 0, 0, 1, 1, 0, 1],  # user 8
+                 [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]]  # user 9
+
+assert friend_matrix[0][2] == 1, "0 and 2 are friends"
+assert friend_matrix[0][8] == 0, "0 and 8 are not friends"
+
+# only need to look at one row
+friends_of_five = [i
+                   for i, is_friend in enumerate(friend_matrix[5])
+                   if is_friend]
+
 
 # Add these tests to the end of your linear_algebra.py file
 # commit and push the update to GitHub
